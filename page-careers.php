2198af64d21511add71d319c898c40fa580ac42b
Shahidul Islam <ishahin56@gmail.com>
2025-02-23 11:15:13 +0600

    Completed featured box and value boxes section dynamic part

--- page-careers.php
+++ page-careers.php
@@ -12,54 +12,6 @@
 get_header();
 ?>
 
-	<!-- career hero section start -->
-	<!-- <section class="career-hero">
-      <div class="container">
-         <div class="career-content padding">
-            <div class="career-main-content mx-auto text-center">
-               <p class="pretitle-bold">careers on tranchulas</p>
-               <h1>Work with us</h1>
-               <p class="pt-1 mb-4">Explore remote-friendly, flexible opportunities and join our mission to make work life simpler, more pleasant and more productive.</p>
-               <a class="btn btn-primary" href="#">View careers</a>
-            </div>
-            <div class="future-works bg-gradient rounded-5 padding overflow-hidden">
-               <div class="future-works-boxes grid">
-                  <div class="future-works-item d-flex flex-column text-center align-items-center position-relative">
-                     <img class="mb-3" src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                     <div class="d-flex flex-column pt-1">
-                        <h5>Connected</h5>
-                        <p class="p14 pt-1">We come together wherever we are – across time zones, regions,
-                           offices and
-                           screens.
-                        </p>
-                     </div>
-                  </div>
-                  <div class="future-works-item d-flex flex-column text-center align-items-center position-relative">
-                     <img class="mb-3" src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                     <div class="d-flex flex-column pt-1">
-                        <h5>Connected</h5>
-                        <p class="p14 pt-1">We come together wherever we are – across time zones, regions, offices and screens.</p>
-                     </div>
-                  </div>
-                  <div class="future-works-item d-flex flex-column text-center align-items-center position-relative">
-                     <img class="mb-3" src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                     <div class="d-flex flex-column pt-1">
-                        <h5>Connected</h5>
-                        <p class="p14 pt-1">We come together wherever we are – across time zones, regions,
-                           offices and
-                           screens.
-                        </p>
-                     </div>
-                  </div>
-               </div>
-               <div class="future-bottom-text-content text-center mx-auto">
-                  <h2 class="mb-3">Tranchulas is where the future works</h2>
-                  <p class="p16 pt-1 mx-auto text-primary">Every day, we refine, iterate and explore how to make work better for everyone. Join us in creating a better future of work that’s more connected, inclusive and flexible.</p>
-               </div>
-            </div>
-         </div>
-      </div>
-   </section> -->
    <section class="career-hero">
       <div class="container">
          <div class="career-content padding">
@@ -119,24 +71,15 @@ get_header();
       </div>
    </section> <!-- section.career-hero -->
    
-   <!-- image section start -->
-   <section class="grid images-section">
+   <section class="grid future-block-section">
       <div class="blocks-item padding d-flex align-items-center justify-content-end">
          <div class="img-content">
-            <h2 class="text-white">Class aptent taciti sociosqu ad litora torquent per conubia nostra.</h2>
+            <h2 class="text-white"><?php echo esc_html( get_field('future_box_title') ); ?></h2>
          </div>
       </div>
       <div class="blocks-item padding d-flex align-items-center">
-         <div class="img-content">
-            <p class="text-white">We’re dedicated to hiring diverse talent and ensuring that we treat you with respect
-               and support
-               throughout the interview process and once you join Slack.
-               <br>
-               <br>
-
-               We embrace diversity and strive to create conditions that provide everyone with an equal opportunity to
-               thrive. 
-            </p>
+         <div class="img-content text-white">
+            <?php echo wp_kses_post(get_field('future_box_content')); ?>
          </div>
       </div>
    </section>
@@ -145,77 +88,44 @@ get_header();
    <section class="value-section">
       <div class="container">
          <div class="value-content padding">
+            <?php 
+            $core_values = get_field('core_values_section');
+            if($core_values) : ?>
             <div class="value-head">
                <div class="row">
                   <div class="col-lg-6">
-                     <h2 class="mb-3">Our core values</h2>
-                     <p class="p16 pt-1">These are some of the values that we live by as a company. We work by them,
-                        too.
-                        We’re building
-                        a platform and products that we believe in, knowing that there is real value to be gained from
-                        helping people to simplify whatever it is that they do and bring more of themselves to their
-                        work, wherever they are.</p>
+                     <?php if($core_values['title']) : ?>
+                        <h2 class="mb-3"><?php echo esc_html($core_values['title']); ?></h2>
+                     <?php endif; ?>
+                     <?php if($core_values['lead_text']) : ?>
+                        <p class="p16 pt-1"><?php echo esc_html($core_values['lead_text']); ?></p>
+                     <?php endif; ?>
                   </div>
                </div>
             </div>
+            <?php if(have_rows('values')): ?>
             <div class="values">
                <div class="row g-5">
+                  <?php while(have_rows('values')): the_row(); ?>
                   <div class="col-sm-6 col-lg-4">
                      <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
+                        <?php if($icon = get_sub_field('icon')): ?>
+                           <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>">
+                        <?php endif; ?>
                         <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
+                        <?php if($title = get_sub_field('title')): ?>
+                           <h5><?php echo esc_html($title); ?></h5>
+                        <?php endif; ?>
+                        <?php if($description = get_sub_field('description')): ?>
+                           <p><?php echo esc_html($description); ?></p>
+                        <?php endif; ?>
                      </div>
                   </div>
-                  <div class="col-sm-6 col-lg-4">
-                     <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                        <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
-                     </div>
-                  </div>
-                  <div class="col-sm-6 col-lg-4">
-                     <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                        <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
-                     </div>
-                  </div>
-                  <div class="col-sm-6 col-lg-4">
-                     <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                        <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
-                     </div>
-                  </div>
-                  <div class="col-sm-6 col-lg-4">
-                     <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                        <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
-                     </div>
-                  </div>
-                  <div class="col-sm-6 col-lg-4">
-                     <div class="value-item">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/icons/Security.png'; ?>" alt="">
-                        <hr class="opacity-100">
-                        <h5>Empathy</h5>
-                        <p>Donec mollis tincidunt augue, in finibus orci imperdiet quis. In vitae nibh consequat,
-                           condimentum lacus.</p>
-                     </div>
-                  </div>
+                  <?php endwhile; ?>
                </div>
             </div>
+            <?php endif; ?>
+            <?php endif; ?>
          </div>
       </div>
    </section>
